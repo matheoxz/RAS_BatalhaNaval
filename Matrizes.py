7f6d624e1e2dea36b030b4e4316dad68fe3aac31
matheoxz <matheus_sinto@hotmail.com>
2020-07-22 19:54:19 -0300

    separacao de funcoes

--- Matrizes.py
+++ Matrizes.py
@@ -1,11 +1,39 @@
-import Tela
+from random import randint
 
 class Matriz:
-    def __init__(self):
+    coordenadas = []
+    def geraMatriz(self, n):
+        for x in range(n):
+            for y in range(n):
+                yield x,y
+
+    def __init__(self, n):
+        self.n = n
+        self.MatrizPlayer = [(x, y, 0) for x, y in self.geraMatriz(n)]
+        self.MatrizBot = [(x, y, 0) for x, y in self.geraMatriz(n)]
         return
 
-#cometario do matheus
-#comentario do lucas
-#comentario do benso
-#comentario da iris
-#comentario do marcos
+    def alocaNavios(self, player, carrier = 1, battleship = 1, cruiser = 1, submarine = 1, destroyer = 1):
+        #poe navios na matriz de maneira aleatoria
+        #deve checar o tamanho do navio e a quantidade destes
+        #por padrão tem-se um único navio de cada, pode-se mudar pelas configurações do jogo
+        #tipos de navios e seus tamanhos
+            #carrier = 5 espaços
+            #battleship = 4 espaços
+            #cruiser = 3 espaços
+            #submarine = 3 espaços
+            #destroyer = 2 espaços
+        pass
+
+    def checaTiro(self, coordenada):
+        pass
+
+    def geraTiro(self):
+        coordenada = (randint(0, self.n-1), randint(0, self.n-1))
+        while(coordenada in self.coordenadas):
+            coordenada = (randint(0, self.n-1), randint(0, self.n-1))
+        self.coordenadas.append(coordenada)
+        return coordenada
+
+
+
--- Tela.py
+++ Tela.py
@@ -1,1 +1,36 @@
-import tkinter
+from tkinter import Tk, PhotoImage
+import Matrizes
+
+class Tela(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Batalha Naval")
+        self.iconphoto(True, PhotoImage("icons/Battleship.ico"))
+        self.state("normal")
+        self.mainloop()
+
+    def criaMatrizDeBotao(self, player):
+        #pega o valor de n que for dado na tela de menu
+        #cria uma matriz de botoes nxn para o player "player" ou "bot"
+        #de acordo com a matriz gerada após a alocação de navios
+
+        pass
+
+    def criaTelaMenu(self):
+        pass
+
+    def criaTelaConfigurações(self):
+        pass
+    
+    def Sandbox(self):
+        pass
+
+    def PvE(self):
+        pass
+
+    def TelaFinal(self):
+        pass
+
+
+tela = Tela()
+    
